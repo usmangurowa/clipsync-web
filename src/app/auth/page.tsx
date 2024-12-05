8eb06f8c9a8d9926babcaa03723cb3b03d1f3ec1
Vixs101 <elijahvix695@gmail.com>
2024-12-05 19:53:06 +0100

    Feat:worked on the UI of the auth page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,8 +1,115 @@
-import React from "react";
-// TODO: Elijah run the auth page
 
-const AuthPage = () => {
-  return <div>AuthPage</div>;
-};
+"use client"
 
-export default AuthPage;
+import Link from "next/link"
+// import Image from 'next/image'
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
+export default function AuthPage() {
+  return (
+    <div className="grid min-h-screen grid-cols-1 md:grid-cols-2">
+      {/* Left section*/}
+      <div className="hidden bg-black md:block">
+        <div className="absolute left-4 top-4 md:left-8 md:top-8">
+          <Link
+            href="/"
+            className="flex items-center space-x-2 text-lg font-semibold"
+          >
+            <span>Clip Sync</span>
+          </Link>
+        </div>
+      </div>
+
+      {/* Right section - form */}
+      <div className="relative flex flex-col ">
+        <div className="block md:hidden absolute left-4 top-4 md:left-8 md:top-8">
+          <Link
+            href="/"
+            className="flex items-center space-x-2 text-lg font-semibold"
+          >
+            <span>Clip Sync</span>
+          </Link>
+        </div>
+        <div className="absolute right-4 top-3 md:right-8 md:top-6">
+          <Button variant="ghost" asChild>
+            <Link href="/login" className="text-lg">Login</Link>
+          </Button>
+        </div>
+
+        <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24 md:mt-28">
+          <div className="mx-auto w-full max-w-sm">
+            <div className="space-y-6">
+              <div className="space-y-2 text-center">
+                <h1 className="text-2xl font-semibold tracking-tight">
+                  Create an account
+                </h1>
+                <p className="text-sm text-muted-foreground">
+                  Enter your email below to create your account
+                </p>
+              </div>
+
+              <div className="space-y-4">
+                <div className="space-y-2">
+                  <Input
+                    id="email"
+                    placeholder="name@example.com"
+                    type="email"
+                    autoCapitalize="none"
+                    autoComplete="email"
+                    autoCorrect="off"
+                  />
+                </div>
+                <Button className="w-full">
+                  Sign in with Email
+                </Button>
+              </div>
+
+              <div className="relative">
+                <div className="absolute inset-0 flex items-center">
+                  <span className="w-full border-t" />
+                </div>
+                <div className="relative flex justify-center text-xs uppercase">
+                  <span className="bg-background px-2 text-muted-foreground">
+                    or continue with
+                  </span>
+                </div>
+              </div>
+
+              <Button variant="outline" className="w-full" type="button">
+                <svg
+                  className="mr-2 h-4 w-4"
+                  aria-hidden="true"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                </svg>
+                GitHub
+              </Button>
+
+              <p className="px-8 text-center text-sm text-muted-foreground">
+                By clicking continue, you agree to our{" "}
+                <Link
+                  href="/terms"
+                  className="underline underline-offset-4 hover:text-primary"
+                >
+                  Terms of Service
+                </Link>{" "}
+                and{" "}
+                <Link
+                  href="/privacy"
+                  className="underline underline-offset-4 hover:text-primary"
+                >
+                  Privacy Policy
+                </Link>
+
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
