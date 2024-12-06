f8475b9d1f9925807cfac0267017303d3ffd394b
Usman Hassan <usmanhassangu@gmail.com>
2024-12-06 17:42:33 +0100

    Refactor: streamline session update logic and remove commented-out code

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,43 +1,19 @@
 import { type NextRequest } from "next/server";
-import { updateSession } from "@/supabase/middleware";
-// import { createClient } from "@/supabase/server";
+import { updateSession } from "./supabase/middleware";
 
 export async function middleware(request: NextRequest) {
   return await updateSession(request);
-  // const response = await updateSession(request);
-
-  // const newUrl = new URL("/", request.url);
-  // const { pathname } = request.nextUrl;
-
-  // if (pathname === "/") {
-  //   return response;
-  // }
-  // const supabase = await createClient();
-  // const { data } = await supabase.auth.getSession();
-
-  // const session = data?.session || null;
-
-  // if (session && pathname.startsWith("/auth")) {
-  //   return NextResponse.redirect(newUrl.origin);
-  // }
-
-  // if (!session && pathname.startsWith("/clipboard")) {
-  //   const encodedSearchParams = `${pathname.substring(1)}${newUrl.search}`;
-
-  //   const url = new URL("/auth", request.url);
-
-  //   if (encodedSearchParams) {
-  //     url.searchParams.append("return_to", encodedSearchParams);
-  //   }
-
-  //   return NextResponse.redirect(url);
-  // }
-
-  // return response;
 }
 
 export const config = {
   matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
     "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
