ce4a406b3e900b7b270c39fd075707cab684d757
Usman Hassan <usmanhassangu@gmail.com>
2024-12-07 07:24:10 +0100

    Refactor: update middleware to use NextResponse and comment out session update logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,9 @@
-import { type NextRequest } from "next/server";
-import { updateSession } from "./supabase/middleware";
+import { NextResponse, type NextRequest } from "next/server";
+// import { updateSession } from "./supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  // return await updateSession(request);
+  return NextResponse.next(request);
 }
 
 export const config = {
