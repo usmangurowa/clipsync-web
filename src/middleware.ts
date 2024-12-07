50171c43bb8153e6bd7846a4c445c09131ce79fc
Usman Hassan <usmanhassangu@gmail.com>
2024-12-07 07:39:30 +0100

    Refactor: simplify middleware function and comment out session update logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,15 @@
 import { NextResponse, type NextRequest } from "next/server";
 // import { updateSession } from "./supabase/middleware";
 
-export async function middleware(request: NextRequest) {
-  // return await updateSession(request);
+// export async function middleware(request: NextRequest) {
+//   // return await updateSession(request);
+//   return new NextResponse.next(request);
+// }
+
+export function middleware(request: NextRequest) {
+  // if (request.nextUrl.pathname === "/blocked") {
+
+  // }
   return NextResponse.next(request);
 }
 
