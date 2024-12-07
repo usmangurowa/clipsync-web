6ff23ccb5383f1c1cace8c33c88dea640b7a1bcc
Usman Hassan <usmanhassangu@gmail.com>
2024-12-07 12:32:05 +0100

    feat: add navigation bar and enhance home page layout with theme toggle and call to action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,69 @@
 "use client";
 import { Logo } from "@/components/brand";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import { useSession } from "@/hooks/use-session";
+import { MoonIcon, SunIcon } from "lucide-react";
+import { useTheme } from "next-themes";
 import Link from "next/link";
 import React from "react";
 
 const Home = () => {
-  const { data } = useSession();
   return (
-    <main className="relative flex h-screen w-screen flex-col items-center justify-center space-y-5">
-      <Logo />
-      <h1 className="text-5xl font-bold">Clip Sync</h1>
-      {data ? (
-        <Link href={"/clipboard"}>Clips</Link>
-      ) : (
-        <Link href={"/auth"}>Login</Link>
-      )}
-    </main>
+    <>
+      <NavBar />
+      <main className="container relative flex h-screen w-screen max-w-3xl flex-col items-center space-y-5 py-10">
+        <Badge variant={"outline"} className="mx-auto">
+          Clip Sync
+        </Badge>
+        <p className="text-center text-5xl font-bold leading-normal">
+          One clipboard to rule them all. Sync your clipboard across all your
+          devices.
+        </p>
+        <Button size={"lg"} asChild>
+          <Link href={"/auth"}>Get Started</Link>
+        </Button>
+      </main>
+    </>
   );
 };
 
 export default Home;
+
+const NavBar = () => {
+  const { data, isLoading } = useSession();
+  const { theme, setTheme } = useTheme();
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+  return (
+    <nav className="border-b">
+      <div className="container flex w-full items-center justify-between">
+        <Link href={"/"} className="flex items-center">
+          <Logo className="scale-50" />
+          <h1 className="text-2xl font-semibold">Clip Sync</h1>
+        </Link>
+        <div className="flex items-center gap-x-2">
+          {!isLoading && (
+            <>
+              <Button size={"sm"} asChild variant={"link"}>
+                {data ? (
+                  <Link href={"/clipboard"}>Clipboard</Link>
+                ) : (
+                  <Link href={"/auth"}>Login</Link>
+                )}
+              </Button>
+              <Button
+                variant={"outline"}
+                size={"icon-sm"}
+                onClick={toggleTheme}
+              >
+                {theme === "dark" ? <SunIcon /> : <MoonIcon />}
+              </Button>
+            </>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
