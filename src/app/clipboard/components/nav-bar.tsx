09131474bf34f32cf9402d3a9f0521ed11acef56
Usman Hassan <usmanhassangu@gmail.com>
2024-12-21 17:53:15 +0100

    feat: add NavBar component for improved layout and integrate search functionality

---
+++ src/app/clipboard/components/nav-bar.tsx
@@ -0,0 +1,21 @@
+"use client";
+import SearchInput from "@/components/search-input";
+import { SwitchThemeButton } from "@/components/switch-theme-button";
+import { useQueryState } from "nuqs";
+import React from "react";
+
+const NavBar = () => {
+  const [q, setQ] = useQueryState("q", { defaultValue: "" });
+  return (
+    <>
+      <nav className="fixed flex h-16 w-full items-center justify-between border-b bg-background px-5">
+        <h1 className="text-2xl font-semibold">Clipboard</h1>
+        <SearchInput value={q} onChange={setQ} />
+        <SwitchThemeButton />
+      </nav>
+      <div className="h-16" />
+    </>
+  );
+};
+
+export default NavBar;
--- src/app/clipboard/page.tsx
+++ src/app/clipboard/page.tsx
@@ -1,23 +1,18 @@
-"use client";
-import React from "react";
+import React, { Suspense } from "react";
 import ClipsBoard from "./components/clipsboard";
-import { SwitchThemeButton } from "@/components/switch-theme-button";
 
-import { useQueryState } from "nuqs";
-import SearchInput from "@/components/search-input";
+import NavBar from "./components/nav-bar";
 
 const Clipboard = () => {
-  const [q, setQ] = useQueryState("q", { defaultValue: "" });
   return (
     <main className="relative h-screen flex-grow">
-      <nav className="fixed flex h-16 w-full items-center justify-between border-b bg-background px-5">
-        <h1 className="text-2xl font-semibold">Clipboard</h1>
-        <SearchInput value={q} onChange={setQ} />
-        <SwitchThemeButton />
-      </nav>
-      <div className="h-16" />
+      <Suspense>
+        <NavBar />
+      </Suspense>
       <section className="p-5">
-        <ClipsBoard />
+        <Suspense>
+          <ClipsBoard />
+        </Suspense>
       </section>
     </main>
   );
