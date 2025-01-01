6709374e1218cbf79d80c8c8ab2f7cf35a3c850b
Usman Hassan <usmanhassangu@gmail.com>
2025-01-02 00:07:01 +0100

    feat: update metadata title and description for improved clarity and user engagement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,9 @@ import { NuqsAdapter } from "nuqs/adapters/next/app";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Clip Sync",
-  description: "Clip Sync",
+  title: "Clip Sync - Sync & Share Seamlessly",
+  description:
+    "One clipboard, all your devices. Copy on your phone, paste on your laptop, and keep your workflow uninterrupted across all your screens. No more emailing yourself text or resharing links",
 };
 
 export default function RootLayout({
