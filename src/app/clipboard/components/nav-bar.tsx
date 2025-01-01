8b04da2cacc0f712e59dc978d3009bba682c174e
Usman Hassan <usmanhassangu@gmail.com>
2025-01-01 22:46:03 +0100

    feat: enhance NavBar styling with backdrop blur and responsive text size

--- src/app/clipboard/components/nav-bar.tsx
+++ src/app/clipboard/components/nav-bar.tsx
@@ -8,8 +8,8 @@ const NavBar = () => {
   const [q, setQ] = useQueryState("q", { defaultValue: "" });
   return (
     <>
-      <nav className="sticky top-0 z-50 grid w-full grid-flow-dense grid-cols-12 items-center justify-between gap-3 border-b bg-background px-5 py-3">
-        <h1 className="col-span-6 text-2xl font-semibold md:col-span-2">
+      <nav className="sticky top-0 z-50 grid w-full grid-flow-dense grid-cols-12 items-center justify-between gap-3 border-b px-5 py-3 backdrop-blur-md">
+        <h1 className="col-span-6 text-xl font-semibold md:col-span-2 md:text-2xl">
           Clipboard
         </h1>
         <div className="col-span-12 flex items-center justify-center md:col-span-8">
