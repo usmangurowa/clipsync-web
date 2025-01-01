f5d9054d28bb3ff03e546187743192db2203d173
Usman Hassan <usmanhassangu@gmail.com>
2025-01-01 22:18:20 +0100

    fix: change NavBar from fixed to sticky positioning for better scrolling behavior

--- src/app/clipboard/components/nav-bar.tsx
+++ src/app/clipboard/components/nav-bar.tsx
@@ -8,12 +8,11 @@ const NavBar = () => {
   const [q, setQ] = useQueryState("q", { defaultValue: "" });
   return (
     <>
-      <nav className="fixed z-50 flex h-16 w-full items-center justify-between border-b bg-background px-5">
+      <nav className="sticky top-0 z-50 flex h-16 w-full items-center justify-between border-b bg-background px-5">
         <h1 className="text-2xl font-semibold">Clipboard</h1>
         <SearchInput value={q} onChange={setQ} />
         <SwitchThemeButton />
       </nav>
-      <div className="h-16" />
     </>
   );
 };
