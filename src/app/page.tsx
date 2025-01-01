ccacb9993303a6f6ab3be5ec41e4dc2011c7c7fe
Usman Hassan <usmanhassangu@gmail.com>
2025-01-01 23:39:44 +0100

    feat: integrate AnimatedShinyText and BlurIn components for enhanced homepage visuals

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 import { Logo } from "@/components/brand";
 import { SwitchThemeButton } from "@/components/switch-theme-button";
-import { Badge } from "@/components/ui/badge";
+import AnimatedShinyText from "@/components/ui/animated-shiny-text";
+import BlurIn from "@/components/ui/blur-in";
+
 import { Button } from "@/components/ui/button";
 import { useSession } from "@/hooks/use-session";
+import { ArrowRightIcon } from "lucide-react";
 
 import Link from "next/link";
 import React from "react";
@@ -13,13 +16,19 @@ const Home = () => {
     <>
       <NavBar />
       <main className="container relative flex h-screen w-screen max-w-3xl flex-col items-center space-y-5 py-10">
-        <Badge variant={"outline"} className="mx-auto">
-          Clip Sync
-        </Badge>
-        <p className="text-center text-5xl font-bold leading-normal">
-          One clipboard to rule them all. Sync your clipboard across all your
-          devices.
-        </p>
+        <AnimatedShinyText className="inline-flex items-center justify-center rounded-full border px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
+          <span>✨ Clip Sync</span>
+          <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
+        </AnimatedShinyText>
+        <BlurIn
+          word="Sync & Share Seamlessly"
+          className="text-center text-3xl font-bold text-black dark:text-white"
+        />
+        <BlurIn
+          word="One clipboard, all your devices. Copy on your phone, paste on your laptop, and keep your workflow uninterrupted across all your screens. No more emailing yourself text or resharing links"
+          className="text-center !text-base font-normal"
+        />
+
         <Button size={"lg"} asChild>
           <Link href={"/auth"}>Get Started</Link>
         </Button>
@@ -34,7 +43,7 @@ const NavBar = () => {
   const { data, isLoading } = useSession();
 
   return (
-    <nav className="border-b">
+    <nav className="sticky top-0 z-50 border-b backdrop-blur-md">
       <div className="container flex w-full items-center justify-between">
         <Link href={"/"} className="flex items-center">
           <Logo className="scale-50" />
