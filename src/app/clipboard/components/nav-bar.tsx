97ddbac407dd765d17e37c3b002c5c3cd0bc46f9
Usman Hassan <usmanhassangu@gmail.com>
2025-01-01 22:41:28 +0100

    feat: refactor NavBar layout to use grid for improved responsiveness and spacing

--- src/app/clipboard/components/nav-bar.tsx
+++ src/app/clipboard/components/nav-bar.tsx
@@ -8,10 +8,16 @@ const NavBar = () => {
   const [q, setQ] = useQueryState("q", { defaultValue: "" });
   return (
     <>
-      <nav className="sticky top-0 z-50 flex h-16 w-full items-center justify-between border-b bg-background px-5">
-        <h1 className="text-2xl font-semibold">Clipboard</h1>
-        <SearchInput value={q} onChange={setQ} />
-        <SwitchThemeButton />
+      <nav className="sticky top-0 z-50 grid w-full grid-flow-dense grid-cols-12 items-center justify-between gap-3 border-b bg-background px-5 py-3">
+        <h1 className="col-span-6 text-2xl font-semibold md:col-span-2">
+          Clipboard
+        </h1>
+        <div className="col-span-12 flex items-center justify-center md:col-span-8">
+          <SearchInput value={q} onChange={setQ} />
+        </div>
+        <div className="col-span-6 flex justify-end md:col-span-2">
+          <SwitchThemeButton />
+        </div>
       </nav>
     </>
   );
