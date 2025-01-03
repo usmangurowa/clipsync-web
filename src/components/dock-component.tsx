0433856315908969c0e275c1c5830b9c0e8fcc57
Usman Hassan <usmanhassangu@gmail.com>
2025-01-03 01:33:44 +0100

    feat: add logout button to DockComponent for improved user session management

--- src/components/dock-component.tsx
+++ src/components/dock-component.tsx
@@ -1,9 +1,11 @@
+"use client";
 import React from "react";
 
 import { Dock, DockIcon } from "@/components/ui/dock";
 import AddToClipboard from "./add-to-clipboard";
 import { Button } from "./ui/button";
-import { SettingsIcon } from "lucide-react";
+import { LogOutIcon, SettingsIcon } from "lucide-react";
+import { logout } from "@/actions/auth";
 
 export type IconProps = React.HTMLAttributes<SVGElement>;
 
@@ -21,6 +23,16 @@ const DockComponent = () => {
             size={"icon"}
           >
             <SettingsIcon />
+          </Button>
+        </DockIcon>
+        <DockIcon>
+          <Button
+            onClick={() => logout()}
+            variant={"outline"}
+            className="size-8 rounded-full"
+            size={"icon"}
+          >
+            <LogOutIcon />
           </Button>
         </DockIcon>
       </Dock>
