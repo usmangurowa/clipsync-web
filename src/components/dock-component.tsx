bf2ddefde3452790f8791b1add32e5961b6ec718
Usman Hassan <usmanhassangu@gmail.com>
2025-01-04 23:31:50 +0100

    feat: add tooltip functionality to dock component and implement dialog component

--- src/components/dock-component.tsx
+++ src/components/dock-component.tsx
@@ -4,8 +4,15 @@ import React from "react";
 import { Dock, DockIcon } from "@/components/ui/dock";
 import AddToClipboard from "./add-to-clipboard";
 import { Button } from "./ui/button";
-import { LogOutIcon, SettingsIcon } from "lucide-react";
+import { LogOutIcon, PencilLineIcon, SettingsIcon } from "lucide-react";
 import { logout } from "@/actions/auth";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export type IconProps = React.HTMLAttributes<SVGElement>;
 
@@ -14,26 +21,43 @@ const DockComponent = () => {
     <div className="fixed bottom-10 left-1/2 z-50 -translate-x-1/2">
       <Dock direction="middle">
         <DockIcon className="relative">
-          <AddToClipboard className="relative bottom-auto right-auto size-8" />
+          <ToolTip content="Paste content to clipboard">
+            <AddToClipboard className="relative bottom-auto right-auto size-8" />
+          </ToolTip>
+        </DockIcon>
+        <DockIcon className="relative">
+          <ToolTip content="Write content to clipboard">
+            <Button
+              variant={"outline"}
+              className="size-8 rounded-full"
+              size={"icon"}
+            >
+              <PencilLineIcon />
+            </Button>
+          </ToolTip>
         </DockIcon>
         <DockIcon>
-          <Button
-            variant={"outline"}
-            className="size-8 rounded-full"
-            size={"icon"}
-          >
-            <SettingsIcon />
-          </Button>
+          <ToolTip content="Settings">
+            <Button
+              variant={"outline"}
+              className="size-8 rounded-full"
+              size={"icon"}
+            >
+              <SettingsIcon />
+            </Button>
+          </ToolTip>
         </DockIcon>
         <DockIcon>
-          <Button
-            onClick={() => logout()}
-            variant={"outline"}
-            className="size-8 rounded-full"
-            size={"icon"}
-          >
-            <LogOutIcon />
-          </Button>
+          <ToolTip content="Logout">
+            <Button
+              onClick={() => logout()}
+              variant={"outline"}
+              className="size-8 rounded-full"
+              size={"icon"}
+            >
+              <LogOutIcon />
+            </Button>
+          </ToolTip>
         </DockIcon>
       </Dock>
     </div>
@@ -41,3 +65,20 @@ const DockComponent = () => {
 };
 
 export default DockComponent;
+
+const ToolTip = ({
+  children,
+  content,
+}: {
+  children: React.ReactNode;
+  content: React.ReactNode;
+}) => (
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger>{children}</TooltipTrigger>
+      <TooltipContent>
+        <p>{content}</p>
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+);
