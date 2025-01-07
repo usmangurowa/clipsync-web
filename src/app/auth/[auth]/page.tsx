733639abc81f5f2c256a48d7f6f2a8a34912f483
Usman Hassan <usmanhassangu@gmail.com>
2025-01-07 09:10:58 +0100

    feat: refactor authentication layout and remove deprecated AuthPage component

--- src/app/auth/[auth]/page.tsx
+++ src/app/auth/[auth]/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 import Link from "next/link";
 
-import { Logo } from "@/components/brand";
-
 import { EmailAuth } from "../components/email-auth";
 import { GithubAuth } from "../components/github-auth";
 import { EmailPasswordAuth } from "../components/email-password-auth";
@@ -23,128 +21,112 @@ export default function AuthPage(props: PageProps) {
   const { auth } = use(props.params) as { auth: "sign-in" | "sign-up" };
   const { lastLoginOption } = useAppConfigStore();
   return (
-    <div className="grid min-h-screen grid-cols-1 md:grid-cols-2">
-      <div className="hidden bg-card md:block">
-        <div className="absolute left-4 top-4 md:left-8 md:top-8">
-          <Link href="/">
-            <Logo />
-          </Link>
-        </div>
-      </div>
-
-      {/* Right section - form */}
-      <div className="relative flex flex-col">
-        <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 md:mt-28 lg:flex-none lg:px-20 xl:px-24">
-          <div className="mx-auto w-full max-w-sm">
-            <div className="space-y-6">
-              <div className="space-y-2 text-center">
-                <h1 className="text-2xl font-semibold tracking-tight">
-                  Clip Sync
-                </h1>
-                <p className="text-sm text-muted-foreground">
-                  {auth === "sign-in"
-                    ? "Sign in to your account"
-                    : "Create an account"}
-                </p>
-              </div>
-              {auth === "sign-up" ? (
-                <>
-                  <EmailPasswordAuth type={auth} />
-                </>
-              ) : (
-                <>
-                  {lastLoginOption === "email" && <EmailAuth />}
-                  {lastLoginOption === "github" && <GithubAuth />}
-                  {(lastLoginOption === "email-password" ||
-                    lastLoginOption === "") && (
-                    <EmailPasswordAuth type={auth} />
-                  )}
-                </>
-              )}
-
-              <Accordion
-                type="single"
-                collapsible
-                defaultValue={lastLoginOption === "" ? "item-1" : ""}
-              >
-                <AccordionItem value="item-1" className="border-none">
-                  <AccordionTrigger
-                    className="justify-center"
-                    indicator={false}
-                  >
-                    <div className="relative w-full">
-                      <div className="absolute inset-0 flex w-full items-center">
-                        <span className="w-full border-t" />
-                      </div>
-                      <span className="relative mx-auto flex w-fit justify-center gap-x-2 bg-background px-2 text-center text-xs uppercase text-muted-foreground">
-                        More options{" "}
-                        <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
-                      </span>
-                    </div>
-                  </AccordionTrigger>
-                  <AccordionContent className="space-y-5">
-                    {auth === "sign-up" ? (
-                      <>
-                        <GithubAuth />
-                        <TextSeparator text={"Email only"} />
-                        <EmailAuth />
-                      </>
-                    ) : (
-                      <>
-                        {lastLoginOption === "email" && (
-                          <>
-                            <GithubAuth />
-                            <TextSeparator text={"Email and password"} />
-                            <EmailPasswordAuth type={auth} />
-                          </>
-                        )}
-                        {lastLoginOption === "github" && (
-                          <>
-                            <TextSeparator text={"Email and password"} />
-                            <EmailPasswordAuth type={auth} />
-                            <TextSeparator text={"Email only"} />
-                            <EmailAuth />
-                          </>
-                        )}
-                        {(lastLoginOption === "email-password" ||
-                          lastLoginOption === "") && (
-                          <>
-                            <GithubAuth />
-                            <TextSeparator text={"Email only"} />
-                            <EmailAuth />
-                          </>
-                        )}
-                      </>
-                    )}
-                  </AccordionContent>
-                </AccordionItem>
-              </Accordion>
-
-              <Button asChild className="w-full" variant={"link"}>
-                <Link
-                  href={`/auth/${auth === "sign-in" ? "sign-up" : "sign-in"}`}
-                >
-                  {auth === "sign-in" ? "Sign up" : "Sign in"}
-                </Link>
-              </Button>
-
-              <p className="px-8 text-center text-sm text-muted-foreground">
-                By clicking continue, you agree to our{" "}
-                <Link
-                  href="/terms"
-                  className="underline underline-offset-4 hover:text-primary"
-                >
-                  Terms of Service
-                </Link>{" "}
-                and{" "}
-                <Link
-                  href="/privacy"
-                  className="underline underline-offset-4 hover:text-primary"
-                >
-                  Privacy Policy
-                </Link>
+    <div className="relative flex flex-col">
+      <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 md:mt-28 lg:flex-none lg:px-20 xl:px-24">
+        <div className="mx-auto w-full max-w-sm">
+          <div className="space-y-6">
+            <div className="space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">
+                Clip Sync
+              </h1>
+              <p className="text-sm text-muted-foreground">
+                {auth === "sign-in"
+                  ? "Sign in to your account"
+                  : "Create an account"}
               </p>
             </div>
+            {auth === "sign-up" ? (
+              <>
+                <EmailPasswordAuth type={auth} />
+              </>
+            ) : (
+              <>
+                {lastLoginOption === "email" && <EmailAuth />}
+                {lastLoginOption === "github" && <GithubAuth />}
+                {(lastLoginOption === "email-password" ||
+                  lastLoginOption === "") && <EmailPasswordAuth type={auth} />}
+              </>
+            )}
+
+            <Accordion
+              type="single"
+              collapsible
+              defaultValue={lastLoginOption === "" ? "item-1" : ""}
+            >
+              <AccordionItem value="item-1" className="border-none">
+                <AccordionTrigger className="justify-center" indicator={false}>
+                  <div className="relative w-full">
+                    <div className="absolute inset-0 flex w-full items-center">
+                      <span className="w-full border-t" />
+                    </div>
+                    <span className="relative mx-auto flex w-fit justify-center gap-x-2 bg-background px-2 text-center text-xs uppercase text-muted-foreground">
+                      More options{" "}
+                      <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
+                    </span>
+                  </div>
+                </AccordionTrigger>
+                <AccordionContent className="space-y-5">
+                  {auth === "sign-up" ? (
+                    <>
+                      <GithubAuth />
+                      <TextSeparator text={"Email only"} />
+                      <EmailAuth />
+                    </>
+                  ) : (
+                    <>
+                      {lastLoginOption === "email" && (
+                        <>
+                          <GithubAuth />
+                          <TextSeparator text={"Email and password"} />
+                          <EmailPasswordAuth type={auth} />
+                        </>
+                      )}
+                      {lastLoginOption === "github" && (
+                        <>
+                          <TextSeparator text={"Email and password"} />
+                          <EmailPasswordAuth type={auth} />
+                          <TextSeparator text={"Email only"} />
+                          <EmailAuth />
+                        </>
+                      )}
+                      {(lastLoginOption === "email-password" ||
+                        lastLoginOption === "") && (
+                        <>
+                          <GithubAuth />
+                          <TextSeparator text={"Email only"} />
+                          <EmailAuth />
+                        </>
+                      )}
+                    </>
+                  )}
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+
+            <Button asChild className="w-full" variant={"link"}>
+              <Link
+                href={`/auth/${auth === "sign-in" ? "sign-up" : "sign-in"}`}
+              >
+                {auth === "sign-in" ? "Sign up" : "Sign in"}
+              </Link>
+            </Button>
+
+            <p className="px-8 text-center text-sm text-muted-foreground">
+              By clicking continue, you agree to our{" "}
+              <Link
+                href="/terms"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Terms of Service
+              </Link>{" "}
+              and{" "}
+              <Link
+                href="/privacy"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Privacy Policy
+              </Link>
+            </p>
           </div>
         </div>
       </div>
--- src/app/auth/verify/page.tsx
+++ src/app/auth/verify/page.tsx
@@ -1,10 +1,6 @@
 "use client";
 
-import Link from "next/link";
-
 import { Button } from "@/components/ui/button";
-
-import { Logo } from "@/components/brand";
 
 import { useAction } from "next-safe-action/hooks";
 import { verify_otp } from "@/actions/auth";
@@ -34,68 +30,57 @@ export default function VerifyPage(props: PageProps) {
     execute({ email, otp });
   };
   return (
-    <div className="grid min-h-screen grid-cols-1 md:grid-cols-2">
-      <div className="hidden bg-card md:block">
-        <div className="absolute left-4 top-4 md:left-8 md:top-8">
-          <Link href="/">
-            <Logo />
-          </Link>
-        </div>
-      </div>
+    <div className="relative flex flex-col">
+      <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 md:mt-28 lg:flex-none lg:px-20 xl:px-24">
+        <div className="mx-auto w-full max-w-sm">
+          <div className="space-y-6">
+            <div className="space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">
+                Verify your email
+              </h1>
+              <p className="text-sm text-muted-foreground">
+                Enter the OTP sent to {email}.
+              </p>
+            </div>
 
-      {/* Right section - form */}
-      <div className="relative flex flex-col">
-        <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 md:mt-28 lg:flex-none lg:px-20 xl:px-24">
-          <div className="mx-auto w-full max-w-sm">
-            <div className="space-y-6">
-              <div className="space-y-2 text-center">
-                <h1 className="text-2xl font-semibold tracking-tight">
-                  Verify your email
-                </h1>
-                <p className="text-sm text-muted-foreground">
-                  Enter the OTP sent to {email}.
-                </p>
-              </div>
-
-              <div className="flex flex-col items-center justify-center space-y-4">
-                <div className="space-y-2">
-                  <InputOTP
-                    maxLength={6}
-                    className="mx-auto"
-                    value={otp}
-                    onChange={setOtp}
-                  >
-                    <InputOTPGroup>
-                      <InputOTPSlot index={0} />
-                      <InputOTPSlot index={1} />
-                      <InputOTPSlot index={2} />
-                    </InputOTPGroup>
-                    <InputOTPSeparator />
-                    <InputOTPGroup>
-                      <InputOTPSlot index={3} />
-                      <InputOTPSlot index={4} />
-                      <InputOTPSlot index={5} />
-                    </InputOTPGroup>
-                  </InputOTP>
-                </div>
-
-                <Button
-                  onClick={handleVerify}
-                  className="w-full"
-                  disabled={status === "executing"}
+            <div className="flex flex-col items-center justify-center space-y-4">
+              <div className="space-y-2">
+                <InputOTP
+                  maxLength={6}
+                  className="mx-auto"
+                  value={otp}
+                  onChange={setOtp}
                 >
-                  Verify
-                </Button>
+                  <InputOTPGroup>
+                    <InputOTPSlot index={0} />
+                    <InputOTPSlot index={1} />
+                    <InputOTPSlot index={2} />
+                  </InputOTPGroup>
+                  <InputOTPSeparator />
+                  <InputOTPGroup>
+                    <InputOTPSlot index={3} />
+                    <InputOTPSlot index={4} />
+                    <InputOTPSlot index={5} />
+                  </InputOTPGroup>
+                </InputOTP>
               </div>
 
               <Button
+                onClick={handleVerify}
                 className="w-full"
-                variant={"link"}
-                onClick={() => router.back()}
+                disabled={status === "executing"}
               >
-                Go Back
+                Verify
               </Button>
             </div>
+
+            <Button
+              className="w-full"
+              variant={"link"}
+              onClick={() => router.back()}
+            >
+              Go Back
+            </Button>
           </div>
         </div>
       </div>
